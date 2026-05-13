5ffd7ebc24c9b5acbf7c824df1d3b45eb352f8f3
jhonatanmsc <jhonmscosta@gmail.com>
2026-05-13 19:44:35 +0100

    fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://0.0.0.0:5173",
         "http://frontend:5173",  # Para Docker
     ],
     allow_credentials=True,
